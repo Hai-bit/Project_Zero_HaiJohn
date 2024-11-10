60ef2179de3653549a1ee3ef1b88dc79f12cab7a
Hai-bit <haiwork14@gmail.com>
2024-11-10 13:49:33 +0100

    Rewritten so that testervariant use the common code. Also change revert the list so it can go both changes on Radius and length of the soleniod. 80 precent completed

--- testervariert.py
+++ testervariert.py
@@ -1,48 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from numba import njit, prange
 
+from common import beregn_B_felt
 # Konstanter
 mu0 = 4 * np.pi * 1e-7  # Permeabiliteten i vakuum
 I = 1.0  # Strøm gjennom solenoiden
 N = 100  # Antall viklinger
 # Parametere for solenoiden for med sikker nummer
 antall_punkter = 5000
-# Numba-optimalisert bfieldlist-funksjon
-@njit
-def bfieldlist(r, koordinater):
-    # Bruker den B-felt fra pensum Chapter 11.1
-    # "Elementary Electromagnetism Using Python"
-    B = np.zeros(3)
-    N = koordinater.shape[0]
-    for i in range(N):
-        i0 = i
-        i1 = (i + 1) % N  # Sikrer at vi går tilbake til start
-        midtpunkt = 0.5 * (
-            koordinater[i1] + koordinater[i0]
-        )  # Midtpunktet av segmentet
-        R_vec = r - midtpunkt
-        dlv = koordinater[i1] - koordinater[i0]  # Differensial lengdevektor
-        norm_R = np.linalg.norm(R_vec)
-        dB = (mu0 * I / (4 * np.pi)) * np.cross(dlv, R_vec) / norm_R ** 3
-        B += dB
-    return B
-
-
-# Numba-optimalisert funksjon for å beregne magnetfeltet over gridet
-@njit(parallel=True)
-def beregn_B_felt(X, Y, Z, koordinater):
-    Bx = np.zeros_like(X)
-    By = np.zeros_like(X)
-    Bz = np.zeros_like(X)
-    for i in prange(X.shape[0]):
-        for j in range(X.shape[1]):
-            r = np.array([X[i, j], Y[i, j], Z[i, j]])
-            B = bfieldlist(r, koordinater)
-            Bx[i, j] = B[0]
-            By[i, j] = B[1]
-            Bz[i, j] = B[2]
-    return Bx, By, Bz
 
 
 # Funksjon for plotting
@@ -82,51 +47,67 @@ y = np.linspace(-line, line, steg)
 
 
 """ 
-Endringer skal gjøres rundt her 
+Endringer på lengde eller radiusen gjøres på 
+radius_liste og lengde_liste
 """
-# Liste over forskjellige radius og lengder
-radius_liste = [1.0]
-lengde_liste = [3.0, 5.0, 7.0]
+# Liste over forskjellige radius og lengder samt standarer
+radius_liste = [1.0, 2.0, 3.0] # endre disse for endre r
+standard_radius = [1.0]
+lengde_liste = [3.0, 5.0, 7.0] # endre disse for endre lengden
+standard_lengde = [5.0]
 
-for L in lengde_liste:
-    for R in radius_liste:
-        # Oppdaterer antall viklinger per lengdeenhet
-        n = N / L
-        radius = R
-        lengde = L
-        # Oppretter koordinatene til solenoiden
-        theta = np.linspace(0, 2 * np.pi * N, antall_punkter)
-        z_solenoid = np.linspace(-lengde / 2, lengde / 2, antall_punkter)
-        x_solenoid = radius * np.cos(theta)
-        y_solenoid = radius * np.sin(theta)
-        koordinater = np.column_stack((x_solenoid, y_solenoid, z_solenoid))
+def cal(L_liste,
+    R_liste): 
+    """
+     Hoved kalklulasjonene til å plotte ut B-feltet
+    R_liste: array
+        Listen til vedlagt radius
+    L_liste : array
+        Listen til vedlagt lengde
+    """
+    for L in L_liste:
+        for R in R_liste:
+            # Oppdaterer antall viklinger per lengdeenhet
+            n = N / L
+            radius = R
+            lengde = L
+            # Oppretter koordinatene til solenoiden
+            theta = np.linspace(0, 2 * np.pi * N, antall_punkter)
+            z_solenoid = np.linspace(-lengde / 2, lengde / 2, antall_punkter)
+            x_solenoid = radius * np.cos(theta)
+            y_solenoid = radius * np.sin(theta)
+            koordinater = np.column_stack((x_solenoid, y_solenoid, z_solenoid))
 
-        # *** Plot i XZ-planet ***
-        # Definerer grid i XZ-planet
-        X_grid, Z_grid = np.meshgrid(x, z)
-        Y_grid = np.zeros_like(X_grid)
+            # *** Plot i XZ-planet ***
+            # Definerer grid i XZ-planet
+            X_grid, Z_grid = np.meshgrid(x, z)
+            Y_grid = np.zeros_like(X_grid)
 
-        # Beregner magnetfeltet ved hvert punkt i gridet
-        Bx, By, Bz = beregn_B_felt(X_grid, Y_grid, Z_grid, koordinater)
+            # Beregner magnetfeltet ved hvert punkt i gridet
+            Bx, By, Bz = beregn_B_felt(X_grid, Y_grid, Z_grid, koordinater)
 
-        navnXZ = ["XZ", "x", "z"]
-        plottingsone(Bx, Bz, X_grid, Z_grid, navnXZ, "b", R, L)
-        # Tegner solenoiden
-        plt.fill_between([-R, R], -L / 2, L / 2, color="gray", alpha=0.3)
+            navnXZ = ["XZ", "x", "z"]
+            plottingsone(Bx, Bz, X_grid, Z_grid, navnXZ, "b", R, L)
+            # Tegner solenoiden
+            plt.fill_between([-R, R], -L / 2, L / 2, color="gray", alpha=0.3)
 
-        # *** Plot i XY-planet ***
-        # Definerer grid i XY-planet
-        X_grid, Y_grid = np.meshgrid(x, y)
-        Z_grid = np.zeros_like(X_grid)
+            # *** Plot i XY-planet ***
+            # Definerer grid i XY-planet
+            X_grid, Y_grid = np.meshgrid(x, y)
+            Z_grid = np.zeros_like(X_grid)
 
-        # Beregner magnetfeltet ved hvert punkt i gridet
-        Bx, By, Bz = beregn_B_felt(X_grid, Y_grid, Z_grid, koordinater)
+            # Beregner magnetfeltet ved hvert punkt i gridet
+            Bx, By, Bz = beregn_B_felt(X_grid, Y_grid, Z_grid, koordinater)
 
-        navnXY = ["XY", "x", "y"]
-        plottingsone(Bx, By, X_grid, Y_grid, navnXY, "r", R, L)
-        # Tegner solenoiden
-        sirkel = plt.Circle((0, 0), R, color="gray", alpha=0.3)
-        plt.gca().add_artist(sirkel)
-        plt.axis("equal")
-
+            navnXY = ["XY", "x", "y"]
+            plottingsone(Bx, By, X_grid, Y_grid, navnXY, "r", R, L)
+            # Tegner solenoiden
+            sirkel = plt.Circle((0, 0), R, color="gray", alpha=0.3)
+            plt.gca().add_artist(sirkel)
+            plt.axis("equal")
+"""
+Kalkulerer B feltet ved forskjellige typer
+"""
+cal(lengde_liste,standard_radius)
+cal(standard_lengde,radius_liste)
 plt.show()
